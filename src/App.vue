a0ccd0777ac1cbb536cc13585824b4770b4534d6
GproSoftware828 <JetSendProductions@Gmail.com>
2019-07-30 18:47:28 +1000

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>Ordered By Storge</h1>
+    <ol>
+      <li v-for="pipe in pipes">
+        {{pipe.color}} {{pipe.length}}
+      </li>
+    </ol>
+    <h2>Last In, First Out:</h2>
+    <Filters @retrievalOrder="reverseOrder" @resetOrder="originalOrder"></Filters>
+  </div>
+</template>
+
+<script>
+import Filters from './components/Filters.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      pipes: [
+        {color: 'green', length: 'long'},
+        {color: 'blue', length: 'medium'},
+        {color: 'lightBlue', length: 'long'},
+        {color: 'red', length: 'short'}
+      ]
+    }
+  },
+  methods: {
+    reverseOrder() {
+      this.pipes.reverse()
+    },
+    originalOrder() {
+      this.pipes.reverse()
+    }
+  },
+  components: {
+    Filters
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Filters.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button type="button" @click="retrievalOrder" :disabled="isClicked">Order By Retrieval</button>
+    &nbsp&nbsp<button @click="resetOrder" :disabled="!isClicked">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Filters',
+  data() {
+    return {
+      isClicked: false
+    }
+  },
+  created() {
+    console.log(this.isClicked)
+  },
+  methods: {
+    retrievalOrder: function() {
+      this.$emit('retrievalOrder');
+      this.isClicked = true;
+      console.log(this.isClicked);
+    },
+    resetOrder: function() {
+      this.$emit('resetOrder');
+      this.isClicked = false;
+      console.log(this.isClicked);
+    }
+  },
+  computed: {
+    isDisabled: function() {
+      return !this.isClicked
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
